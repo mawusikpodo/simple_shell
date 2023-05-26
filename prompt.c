288912b5951aeb39a72b3776d4cc5db6fec7a466
Robert Mawusi Kpodo <mawusikpodo@gmail.com>
2023-05-26 05:04:09 -0700

    Fixed error

--- prompt.c
+++ prompt.c
@@ -36,55 +36,6 @@ char *read_command(void)
 	return (line);
 }
 
-
-/**
- * read_line - read a line from the stream
- *
- * Return: pointer that points the the read line
- */
-char *read_line(void)
-{
-	int bufsize = 1024, i = 0, character;
-	char *line = malloc(sizeof(char) * bufsize);
-
-	if (line == NULL)
-	{
-		perror("allocation error in read_stream");
-		free(line);
-		exit(EXIT_FAILURE);
-	}
-	while (1)
-	{
-		character = getchar(); /* read first char from stream */
-		if (character == EOF)
-		{
-			line[i] = '\0';
-			break;
-		}
-		else if (character == '\n')
-		{
-			line[i] = '\0';
-			break;
-		}
-		else
-		{
-			line[i] = character;
-		}
-		i++;
-		if (i >= bufsize)
-		{
-			bufsize += 1024;
-			line = realloc(line, bufsize);
-			if (line == NULL)
-			{
-				perror("reallocation error in read_stream");
-				exit(EXIT_FAILURE);
-			}
-		}
-	}
-	return (line);
-}
-
 /**
  * prompt - main prompt function
  * @envp: environment variables
@@ -94,17 +45,34 @@ void prompt(char **envp)
 	char *line = NULL;
 	char **args;
 
-	while (1)
+	if (isatty(STDIN_FILENO))
 	{
-		print_prompt();
-		if (isatty(STDIN_FILENO) == 1)
+		while (1)
+		{
+			print_prompt();
 			line = read_command();
-		else
-			line = read_line();
+			args = tokenize(line);
+			exec_cmd(args, envp);
+			free(line);
+			free(args);
+		}
+	}
+	else
+	{
+		size_t bufsize = 0;
+		ssize_t nread;
 
-		args = tokenize(line);
-		exec_cmd(args, envp);
-		free(line);
-		free(args);
+		while ((nread = getline(&line, &bufsize, stdin)) != -1)
+		{
+			if (nread > 0 && line[nread - 1] == '\n')
+				line[nread - 1] = '\0';
+			args = tokenize(line);
+			exec_cmd(args, envp);
+			free(line);
+			free(args);
+			line = NULL;
+		}
+		if (line != NULL)
+			free(line);
 	}
 }
